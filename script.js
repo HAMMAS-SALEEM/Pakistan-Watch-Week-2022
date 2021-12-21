a0a7528f3447dec44299caabe956c98c187dd589
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-22 01:46:35 +0500

    Fix linter error for Project

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const burgerBtn = document.querySelector('.ham0')
-const navMenu = document.querySelector('.nav_desktop')
-const crossBtn = document.querySelector('.crossBtn')
+const burgerBtn = document.querySelector('.ham0');
+const navMenu = document.querySelector('.nav_desktop');
+const crossBtn = document.querySelector('.crossBtn');
 
-burgerBtn.addEventListener('click' , ()=>{
-    console.log('Hammas')
-    navMenu.style.visibility = "visible"
-})
+burgerBtn.addEventListener('click', () => {
+  navMenu.style.visibility = 'visible';
+});
 
-function hideMenu () {
-    if(window.innerWidth<769){
-        navMenu.style.visibility = "hidden"
-    }
+function hideMenu() {
+  if (window.innerWidth < 769) {
+    navMenu.style.visibility = 'hidden';
+  }
 }
 
-crossBtn.addEventListener('click' , hideMenu)
-
-
+crossBtn.addEventListener('click', hideMenu);
