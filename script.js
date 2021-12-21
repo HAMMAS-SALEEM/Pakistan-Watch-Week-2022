0083d49eb1cde63a5aac667b63378ca4478fe639
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-22 00:56:51 +0500

    Finished mobile menu for Project

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const burgerBtn = document.querySelector('.ham0')
 const navMenu = document.querySelector('.nav_desktop')
 const crossBtn = document.querySelector('.crossBtn')
-const closeBtn = document.querySelectorAll('a.func_01')
-const func02 = document.querySelectorAll('.func_02')
-const func03 = document.querySelectorAll('.func_03')
-const func04 = document.querySelectorAll('.func_04')
 
 burgerBtn.addEventListener('click' , ()=>{
     console.log('Hammas')
@@ -12,7 +8,9 @@ burgerBtn.addEventListener('click' , ()=>{
 })
 
 function hideMenu () {
-    navMenu.style.visibility = "hidden"
+    if(window.innerWidth<769){
+        navMenu.style.visibility = "hidden"
+    }
 }
 
 crossBtn.addEventListener('click' , hideMenu)
