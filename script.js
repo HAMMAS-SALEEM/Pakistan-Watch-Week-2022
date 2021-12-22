e1475cd388ce8aa5e7e8edd1b38d27f642d9e2bc
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-23 03:41:46 +0500

    Update script of Array (After First Submission)

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ const crossBtn = document.querySelector('.crossBtn');
 const speakerInfo = document.querySelector('.speaker_info');
 
 const data = [
-{
-  speakerImage: 'images/rolex_ceo.jpg',
-  name: 'Jean-Frédéric Dufourr',
-  alt: 'Jean-Frédéric Dufour image',
-  profession: "Citizen Watch's President & CEO",
-  achievements: 'Jean-Frédéric Dufourr is an American business executive who has been the chief executive officer of Rolex Watch Inc. since 2020.',
-},
-{
-  speakerImage: 'images/tudor_ceo.jpg',
-  name: 'Phillipe Peverelli',
-  alt: 'Toshiko Sato image',
-  profession: 'CEO of Tudor',
-  achievements: 'Phillipe Peverelli is an swedish business executive who has been the chief executive officer of Tudor Inc. since 2010.',
-},
-{
-  speakerImage: 'images/citizen_ceo.jpg',
-  name: 'Toshihiko Sato',
-  alt: 'Toshiko Sato image',
-  profession: "Citizen Watch's President & CEO",
-  achievements: 'Jean-Frédéric Dufourr is an American business executive who has been the chief executive officer of Rolex Watch Inc. since 2020.',
-},
-{
-  speakerImage: 'images/apple_ceo.jpg',
-  name: 'Tim Cook',
-  alt: 'Tim Cook image',
-  profession: "Citizen Watch's President & CEO",
-  achievements: 'Timothy Donald Cook is an American business executive who has been the chief executive officer of Apple Inc. since 2011.',
-}
+  {
+    speakerImage: 'images/rolex_ceo.jpg',
+    name: 'Jean-Frédéric Dufourr',
+    alt: 'Jean-Frédéric Dufour image',
+    profession: "Citizen Watch's President & CEO",
+    achievements: 'Jean-Frédéric Dufourr is an American business executive who has been the chief executive officer of Rolex Watch Inc. since 2020.',
+  },
+  {
+    speakerImage: 'images/tudor_ceo.jpg',
+    name: 'Phillipe Peverelli',
+    alt: 'Toshiko Sato image',
+    profession: 'CEO of Tudor',
+    achievements: 'Phillipe Peverelli is an swedish business executive who has been the chief executive officer of Tudor Inc. since 2010.',
+  },
+  {
+    speakerImage: 'images/citizen_ceo.jpg',
+    name: 'Toshihiko Sato',
+    alt: 'Toshiko Sato image',
+    profession: "Citizen Watch's President & CEO",
+    achievements: 'Jean-Frédéric Dufourr is an American business executive who has been the chief executive officer of Rolex Watch Inc. since 2020.',
+  },
+  {
+    speakerImage: 'images/apple_ceo.jpg',
+    name: 'Tim Cook',
+    alt: 'Tim Cook image',
+    profession: "Citizen Watch's President & CEO",
+    achievements: 'Timothy Donald Cook is an American business executive who has been the chief executive officer of Apple Inc. since 2011.',
+  },
 ];
 
 for (let i = 0; i < data.length; i += 1) {
