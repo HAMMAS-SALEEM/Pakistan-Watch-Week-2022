1696a5ccbf59e2ebbc7db430dab13d9efbce1033
Hammas-Saleem <hammassaleem376@gmail.com>
2021-12-22 22:22:43 +0500

    Almost finished Capstone Project

---
+++ about_script.js
@@ -0,0 +1,17 @@
+const burgerBtn = document.querySelector('.ham0');
+const navMenu = document.querySelector('.nav_desktop');
+const crossBtn = document.querySelector('.crossBtn');
+
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.style.visibility = 'visible';
+  });
+  
+  function hideMenu() {
+    if (window.innerWidth < 769) {
+      navMenu.style.visibility = 'hidden';
+    }
+  }
+  
+  crossBtn.addEventListener('click', hideMenu);
+  
